b1756910f41c2cf7bdb84d7798bd883327696035
ibrokhim_ali786 <128887381+ibrokhimali786@users.noreply.github.com>
2023-07-11 17:41:22 +0500

    Completed

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -4,9 +4,7 @@ public class ReassigningValues {
     public static void main(String[] args) {
         int first = 1, second = 10, third = 100;
         System.out.println(first + "\n" + second + "\n" + third);
-        int linkToFirst = 15;
-        int linkToSecond = 6;
-        int linkToThird = 4;
+        int linkToFirst = 15, linkToSecond = 6, linkToThird = 4;
         System.out.println(linkToFirst + "\n" + linkToSecond + "\n" + linkToThird);
         System.out.println(first + "\n" + second + "\n" + third);
     }
